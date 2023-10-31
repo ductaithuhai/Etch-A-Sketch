e0b16afc2ba0afabb92a33f3033ea0cfa3c4cccc
ductaithuhai <nductai94@gmail.com>
2023-10-31 15:08:56 +0700

    first step

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,4 +1,19 @@
 let container=document.getElementById('container');
-let div=document.createElement('div');
-div.setAttribute('class','square');
-container.appendChild(div);
+let square=[];
+for(let i=1;i<257;i++){
+  square[i]=document.createElement('div');
+  square[i].setAttribute('class','square'+[i]);
+  square[i].style.backgroundColor='white';
+  square[i].style.width='40px';
+  square[i].style.height='40px';
+  container.appendChild(square[i]);
+}
+
+for(let i=1;i<257;i++){
+  square[i]=document.createElement('div');
+  square[i].setAttribute('class','square'+[i]);
+  square[i].style.backgroundColor='white';
+  square[i].style.width='40px';
+  square[i].style.height='40px';
+  container.appendChild(square[i]);
+}
