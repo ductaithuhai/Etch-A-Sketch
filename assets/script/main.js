6e58a7d76de6e3e4b762c26c7ddd2e3ac5be624f
ductaithuhai <nductai94@gmail.com>
2023-11-03 10:30:50 +0700

    half way

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,47 +1,105 @@
-let container=document.getElementById('container');
-let square=[];
-for(let i=1;i<257;i++){
-  square[i]=document.createElement('div');
-  square[i].setAttribute('class','square'+[i]);
-  square[i].style.backgroundColor='white';
-  square[i].style.width='40px';
-  square[i].style.height='40px';
-  container.appendChild(square[i]);
-}
 
-function selectColor(event) {
-  var target = event.target;
-  if (target.className === "color") {
-    // Get all the color boxes by their class name
-    var boxes = document.getElementById("color");
-    // Loop through all the boxes
-    for (var i = 0; i < boxes.length; i++) {
-      // Remove the selected class from each box
-      boxes[i].classList.remove("selected");
+let yourSize = document.getElementById("pickSize");
+let size;
+function getSize() {
+  size = document.getElementById("size").value;
+  let container = document.getElementById('container');
+  while (container.lastElementChild) {
+    container.removeChild(container.lastElementChild);
+  }
+  let square = [];
+  if (size == 256) {
+    for (let i = 1; i <= size; i++) {
+      square[i] = document.createElement('div');
+      square[i].setAttribute('class', 'square' + [i]);
+      square[i].style.backgroundColor = 'white';
+      square[i].style.width = '40px';
+      square[i].style.height = '40px';
+      container.appendChild(square[i]);
     }
-    // Add the selected class to the target box
-    target.classList.add("selected");
-    // Get the display element by its id
-    var display = document.getElementById("display");
-    // Set the display text to the target background color
-    display.textContent = target.style.backgroundColor;
-    // Set the display background color to the target background color
-    display.style.backgroundColor = target.style.backgroundColor;
-    console.log(target.style.backgroundColor);
-    let element=[];
-    for (let i=1;i<257;i++){
-      element[i] = document.getElementsByClassName("square"+[i]);
+  } else if (size == 1024) {
+    for (let i = 1; i <= size; i++) {
+      square[i] = document.createElement('div');
+      square[i].setAttribute('class', 'square' + [i]);
+      square[i].style.backgroundColor = 'white';
+      square[i].style.width = '20px';
+      square[i].style.height = '20px';
+      container.appendChild(square[i]);
+    }
+  } else {
+    for (let i = 1; i <= size; i++) {
+      square[i] = document.createElement('div');
+      square[i].setAttribute('class', 'square' + [i]);
+      square[i].style.backgroundColor = 'white';
+      square[i].style.width = '10px';
+      square[i].style.height = '10px';
+      container.appendChild(square[i]);
+    }
+  }
+
+
+  let color;
+  function getVal() {
+    color = document.getElementById("favcolor").value;
+  }
+  function erase() {
+    color = 'white';
+  };
+
+  let element = [];
+  for (let i = 1; i <= size + 1; i++) {
+    element[i] = document.getElementsByClassName("square" + [i]);
     console.log(element);
     console.log(element[1]);
     function changeColor(color) {
       (element[i])[0].style.backgroundColor = color;
+    }
+    (element[i])[0].addEventListener("mouseenter", function () {
+      let yourColor = document.getElementById("pickColor");
+      yourColor.addEventListener("click", getVal);
+      if ((element[i])[0].style.backgroundColor === 'white') {
+        changeColor(color);
       }
-      (element[i])[0].addEventListener("mouseenter", function() {
-      changeColor(target.style.backgroundColor);});
-}
-}
-}
-    let palette = document.getElementById("color");
 
-    console.log(palette);
-    palette.addEventListener("click", selectColor);
+      let eraseAll = document.getElementById('erase');
+      eraseAll.addEventListener("click", erase);
+      if ((element[i])[0].style.backgroundColor !== 'white') {
+        changeColor(color);
+      }
+    });
+  }
+}
+yourSize.addEventListener("click", getSize);
+
+// let container = document.getElementById('container');
+// let square = [];
+// for (let i = 1; i < size + 1; i++) {
+//   square[i] = document.createElement('div');
+//   square[i].setAttribute('class', 'square' + [i]);
+//   square[i].style.backgroundColor = 'white';
+//   square[i].style.width = '40px';
+//   square[i].style.height = '40px';
+//   container.appendChild(square[i]);
+// }
+
+// let color;
+// function getVal() {
+//   color = document.getElementById("favcolor").value;
+// }
+
+// let element = [];
+// for (let i = 1; i < size + 1; i++) {
+//   element[i] = document.getElementsByClassName("square" + [i]);
+//   console.log(element);
+//   console.log(element[1]);
+//   function changeColor(color) {
+//     (element[i])[0].style.backgroundColor = color;
+//   }
+//   (element[i])[0].addEventListener("mouseenter", function () {
+//     let yourColor = document.getElementById("pickColor");
+//     yourColor.addEventListener("click", getVal);
+//     if ((element[i])[0].style.backgroundColor === 'white') {
+//       changeColor(color);
+//     }
+//   });
+// }
